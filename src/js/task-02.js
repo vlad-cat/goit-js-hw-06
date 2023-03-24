f28a8084cc7af19e7acecc2187f279a979bfecb6
vlad-cat <babenko.vlad1327@gmail.com>
2023-03-24 10:14:52 +0200

    Виконав 2 е завдання

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const li = document.querySelector('#ingredients');
+const Elements = ingredients.map(Option => {
+  const item = document.createElement('li');
+  item.textContent = Option;
+  item.classList.add = 'item';
+  return item;
+});
+
+li.append(...Elements);
