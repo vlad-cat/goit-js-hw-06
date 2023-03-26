1d9b8a6a5083ad34fdc44000741fae812c899eeb
vlad-cat <babenko.vlad1327@gmail.com>
2023-03-26 22:36:00 +0300

    Виконав 8 е та 9 е завдання

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+const onFormSubmit = event => {
+  event.preventDefault();
 
+  const emailEl = event.currentTarget.email.value;
+  const passwordEl = event.currentTarget.password.value;
+
+  if (emailEl === '' || passwordEl === '') {
+    return alert('You need to enter email and password!!!');
+  } else {
+    console.log('You have successfully registered)');
+  }
+
+  console.log({ email: emailEl, password: passwordEl });
+
+  form.reset();
+};
+
+form.addEventListener('submit', onFormSubmit);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+};
+console.log(refs);
+const onChangeColor = event => {
+  const color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+  refs.span.textContent = color;
+};
+
+refs.button.addEventListener('click', onChangeColor);
