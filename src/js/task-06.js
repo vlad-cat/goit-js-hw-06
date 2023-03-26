90b630c46d73803541ec5a9bab35eba02848743f
vlad-cat <babenko.vlad1327@gmail.com>
2023-03-26 22:16:39 +0300

    виконав 6 е завдання

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,3 +1,14 @@
 const input = document.querySelector('#validation-input');
 console.log(input);
 
+const onInputBlur = event => {
+  if (Number(input.dataset.length) === event.currentTarget.value.length) {
+    input.classList.remove('invalid');
+    input.classList.add('valid');
+  } else {
+    input.classList.remove('invalid');
+    input.classList.add('valid');
+  }
+};
+
+input.addEventListener('blur', onInputBlur);
